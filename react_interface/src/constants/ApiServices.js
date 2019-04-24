1ac219773265056af1b3de6e3923a0ad3fbd864e
rayjung95 <rayjung95@gmail.com>
2019-04-24 07:12:40 -0700

    delete api key

--- react_interface/src/constants/ApiServices.js
+++ react_interface/src/constants/ApiServices.js
@@ -9,12 +9,6 @@ export const client = {
   }
 }
 
-// Api-key and Token
-export const API_KEY = 'j05wd2ae49d212578ef13cb607cef64b';
-export const SESSION_TOKEN = '7GJZh/PzWvDabpAIqw5qnKY/dvjc/3Xe';
-//export const SESSION_TOKEN = 'Kw/xlaGwyV/6mmf6CF2oxo9Y4eqokxzO';
-export const SONGKICK_API_KEY = 'mzzfkojpy82tOJLz';
-export const GOOGLE_API_KEY = 'AIzaSyCdkuIQGc6zBWg22z3i7EalpRQL_79RLjU';
 
 // Header
 export const HEADERS = {
