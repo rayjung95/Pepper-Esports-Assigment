f21e71426f7b36b4fbe9c8525fa058dcc98970ad
rayjung95 <rayjung95@gmail.com>
2019-04-26 10:08:36 -0700

    add readme and write test

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Pepper-Esports-Assigment
+This project is an assignment that requires **django backend api server** with **react.js frontend** to create Todo App
+
+## Scripts Structure
+
+```
+my-app
+├── README.md
+├── django
+└── react_interface
+```
+### Installing and Running Django API server  - Backend
+
+**You’ll need to have Python 3.7.x and Django 2.x on your local development machine**
+
+```sh
+cd django
+pipenv install --dev
+
+cd todo_project
+python3 manage.py runserver
+```
+It will start django api server which allow **POST, DELETE, GET Methods** on **http://127.0.0.1:8000**
+
+### `python3 manage.py test`
+
+
+```sh
+cd todo_project
+python3 manage.py test
+```
+
+It will run the test which includes <br>
+- Test for **GET-API** all Todo Items
+- Test for **POST-API** a Todo Item
+- Test for **DELETE-API** a Todo Item
+
+
+### Installing and Running React.js App - Frontend
+
+```sh
+cd react_interface
+npm install or yarn install
+npm start or yarn start
+```
+It will start React.js Todo App on **http://localhost:3000**
--- django/todo_project/todo/test.py
+++ django/todo_project/todo/test.py
@@ -1,12 +1,48 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 from .models import TodoItem
+from django.urls import reverse
+
 
 class TodoItemTestCase(TestCase):
     def setUp(self):
-        TodoItem.objects.create(state="1", content="test1", due_date="2018-01-01")
-        TodoItem.objects.create(state="3", content="test2", due_date="2018-01-02")
+        TodoItem.objects.create(
+            state="1", content="test1", due_date="2018-01-01")
+        TodoItem.objects.create(
+            state="3", content="test2", due_date="2018-01-02")
+
     def test_animals_can_speak(self):
         test1 = TodoItem.objects.get(content="test1")
         test2 = TodoItem.objects.get(content="test2")
         self.assertEqual(test1.showState(), 1)
         # self.assertEqual(cat.speak(), 'The cat says "meow"')
+
+
+class TodoItemAPITestCase(APITestCase):
+    def setUp(self):
+        TodoItem.objects.create(
+            state="1", content="test1", due_date="2018-01-01")
+        TodoItem.objects.create(
+            state="3", content="test2", due_date="2018-01-02")
+
+    def test_get_alltodo(self):
+        client = APIClient()
+        response = client.get('/api/todoItems/', {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_todo(self):
+        client = APIClient()
+        data = {
+            "state": 1,
+            "content": "new created data",
+            "due_date": "2019-01-09T00:00:00Z"
+        }
+        response = client.post('/api/todoItems/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_delete_todo(self):
+        client = APIClient()
+        response = client.delete('/api/todoItems/1/', {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
